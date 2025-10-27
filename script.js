5debe622961c236ace74367f44d78021577098c4
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 00:36:28 +0300

    scrip js eklendi

---
+++ script.js
@@ -0,0 +1,19 @@
+const div = document.getElementById("icerik");
+    const buton = document.getElementById("gonder");
+
+    buton.addEventListener("click", async () => {
+      try {
+        // Sunucuya div içeriğini gönder
+        const response = await fetch("/echo", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ mesaj: div.innerText })
+        });
+
+        const data = await response.json();
+        // Geri gelen mesajı div içinde göster
+        div.innerText = data.mesaj;
+      } catch (err) {
+        div.innerText = "Hata: " + err.message;
+      }
+    });
