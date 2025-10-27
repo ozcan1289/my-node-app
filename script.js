4bb6738d12c0d3b33d2e7db9b9cd194ffca1b401
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 00:41:24 +0300

    sadelestirildi

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 const div = document.getElementById("icerik");
 
-    buton.addEventListener("click", async () => {
-      try {
-        // Sunucuya div içeriğini gönder
-        const response = await fetch("/echo", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ mesaj: div.innerText })
-        });
-
-        const data = await response.json();
-        // Geri gelen mesajı div içinde göster
-        div.innerText = data.mesaj;
-      } catch (err) {
-        div.innerText = "Hata: " + err.message;
-      }
-    });
--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Tek div ile gönderilen mesajı geri döndüren endpoint
-app.post("/echo", (req, res) => {
-  const { mesaj } = req.body;
-  res.json({ mesaj: mesaj || "Boş mesaj gönderildi" });
-});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor 🚀`));
