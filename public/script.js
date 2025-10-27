cf3f05b281f1ce4cbcf29a5c292afa7f484aa58c
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 01:34:15 +0300

    form denemesi

--- public/script.js
+++ public/script.js
@@ -1,7 +1,22 @@
 // Div ve buton referansları
 const div = document.getElementById("icerik");
+const form = document.getElementById("form");
+const sonucDiv = document.getElementById("sonuc");
 
 
 setTimeout(() => {
   div.innerText = "Merhaba! Şimdi JS ile değiştirildi 🎉";
 }, 3000);
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Sayfanın yenilenmesini engelle
+
+  const ad = document.getElementById("ad").value;
+  const soyad = document.getElementById("soyad").value;
+
+  sonucDiv.innerText = `Gönderilen Bilgi: ${ad} ${soyad}`;
+
+  // İstersen formu sıfırlayabilirsin
+  form.reset();
+});
