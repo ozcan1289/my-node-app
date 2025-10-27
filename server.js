d15b97c85591f4551e46e2fd10ca2bf82c3fc504
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 01:25:23 +0300

    2 farklı html

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// / URL'si için index.html'i göster
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
+// /hakkimizda URL'si için hakkimizda.html'i göster
+app.get("/hakkimizda", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/hakkimizda.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor 🚀`));
