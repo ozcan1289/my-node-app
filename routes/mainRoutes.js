5b0b41fa509529750c194d77ade290dfb8304662
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 15:03:15 +0300

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+res.sendFile(path.join(__dirname, "../public/index.html"));
+
 
 // Rotalar
 app.use("/", mainRoutes);
