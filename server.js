4953c1de9a1a730fd400d35e0a4a578573c322c4
Özcan ÜLGER <ozcan.ulger@gmail.com>
2025-10-28 14:20:06 +0300

    öenmli değişiklik
    önemli

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import mainRoutes from "./routes/mainRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+// Rotaları dahil et
+app.use("/", mainRoutes);
+app.use("/api", userRoutes);
+
+app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor 🚀`));
+
+
+
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const { Pool } = require("pg");
 
@@ -13,55 +33,29 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-
-app.use(bodyParser.json()); // JSON veriyi parse etmek için
-app.use(express.static(path.join(__dirname, "public")));
-
-
-app.post("/kaydet", async (req, res) => {
-  const { ad, soyad } = req.body;
-
-  try {
-    const result = await pool.query(
-      "INSERT INTO kullanicilar (ad, soyad) VALUES ($1, $2) RETURNING *",
-      [ad, soyad]
-    );
-    res.json({ mesaj: "Başarıyla kaydedildi", kullanici: result.rows[0] });
-    console.log("Kullanıcı kaydedildi:", result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ hata: "Kaydedilemedi" });
-  }
-});
-
-app.get("/kullanicilar", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM kullanicilar ORDER BY id DESC");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ hata: "Kullanıcılar getirilemedi" });
-  }
-});
+//app.post("/kaydet", async (req, res) => {
+//  const { ad, soyad } = req.body;
+//
+//  try {
+//    const result = await pool.query(
+//      "INSERT INTO kullanicilar (ad, soyad) VALUES ($1, $2) RETURNING *",
+//      [ad, soyad]
+//    );
+//    res.json({ mesaj: "Başarıyla kaydedildi", kullanici: result.rows[0] });
+//    console.log("Kullanıcı kaydedildi:", result.rows[0]);
+//  } catch (err) {
+//    console.error(err);
+//    res.status(500).json({ hata: "Kaydedilemedi" });
+//  }
+//});
 
 
-// / URL'si için index.html'i göster
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
 
-app.get("/kayit", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/kayit.html"));
-});
 
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/admin.html"));
-});
 
-// /hakkimizda URL'si için hakkimizda.html'i göster
-app.get("/hakkimizda", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/hakkimizda.html"));
-});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor 🚀`));
